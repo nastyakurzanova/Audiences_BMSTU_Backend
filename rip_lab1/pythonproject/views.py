65e5485c2dbfc0d9ff9c943a15fade2e14d15302
nastyakurzanova <71400042+nastyakurzanova@users.noreply.github.com>
2024-01-16 01:55:34 +0300

    Обновлены названия функций

---
+++ rip_lab1/pythonproject/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from datetime import date
+# https://aristos-ekus.ru/image/catalog/seocms/gallery/auditorii-baymana/4_5565dbdc236d9.jpg
+
+# назвать о аудиториях
+room_arr =  [
+            {'title': '501ю', 'id': 501, 'src': 'image/185.jpg','corpus': 'Главное здание','price': 6000, 'info': 'Большая аудитория для интересных лекция'},
+            {'title': '306э', 'id': 306, 'src': 'image/1.jpg','corpus': 'Энерго', 'price': 7500,'info': 'Аудитория для лабораторных'},
+            {'title': '222л', 'id': 222, 'src': 'image/222_1.jpg','corpus': 'УЛК','price': 8000, 'info': 'Лекционная аудитория'},
+            {'title': '362м', 'id': 362, 'src': 'image/362m.jpg','corpus': 'СМ', 'price': 5000,'info': 'аудиториядля лабораторных'},
+            {'title': '111', 'id': 111, 'src': 'image/home.jpg','corpus': 'Главное здание', 'price': 12000,'info': 'Дом физики. Аудитория для лабораторных'},
+            {'title': '903', 'id': 903, 'src': 'image/903.jpg','corpus': 'Главное здание', 'price': 15000,'info': 'Аудитория кафедры ИУ5'},
+            {'title': '135л', 'id': 135, 'src': 'image/ulk.jpeg','corpus': 'УЛК', 'price': 4000,'info': 'Аудитория для семинаров'},
+            {'title': '323', 'id': 323, 'src': 'image/323.jpg','corpus': 'Главное здание','price': 3000, 'info': 'Аудитория для лекций по физике'},
+        ]
+
+# def GetOrders(request):
+#     return render(request, 'orders.html', {'data' : {
+#         'current_date': date.today(),
+#         'orders': info_arr,
+#     }})
+# get room
+def GetRoom(request, id):
+    audience = next((sub for sub in room_arr if sub["id"] == id), None)
+    if audience:
+        print(audience["title"])
+    else:
+        print("Not found!")
+    return render(request, 'audience.html', {'data' : {
+        'current_date': date.today(),
+        'id': id,
+        'audiences': audience,
+    }})
+
+# убрать саб
+def GetRoomSearch(request):
+    input_text = request.GET.get("room")
+    print(input_text)
+    temp_arr = []
+    for i in room_arr:
+        if input_text is not None:
+            if input_text in i['title']:
+                temp_arr.append(i)
+        else:
+            return render(request, 'audiences.html', {'data' : {
+                'audiences': room_arr,
+                'query': input_text,
+            }})
+    return render(request, 'audiences.html', {'data' : {
+        'audiences': temp_arr,
+        'query': input_text,
+    }})
